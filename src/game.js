f2a367aeb1030627c763b862e2b01567ba3efa5a
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-06-12 10:30:23 -0500

    Add changePlayerTurn function

--- src/game.js
+++ src/game.js
@@ -19,7 +19,7 @@ class Game {
     this.isDraw = false;
   }
   //track game Data - current moves are stored in array above
-  //selecting moves and capturing that will occur in main.js and pass argument "moveMade" of button
+  //selecting moves and capturing will occur in main.js and pass argument "moveMade" of button
   recordPlayerMove(moveMade) {
     if (this.turn.id === this.player1.id) {
       this.player1Moves.push(moveMade);
@@ -28,13 +28,15 @@ class Game {
     }
     checkForWin();
     checkForDraw();
-    changePlayerTurn();
   }
-  //track of player turn - change it!  Turn tracker above, could we manipulate it through main.js?
+  //track of player turn. Turn tracker above, invoke function in checkForWin()?
   changePlayerTurn() {
-
+    if (this.turn === this.player1) {
+      this.turn = this.player2;
+    } else {
+      this.turn = this.player1;
+    }
   }
-
   holdWinningMoves(playerMoves) {
     if (playerMoves.includes("a1") && playerMoves.includes("a2") && playerMoves.includes("a3") ||
       playerMoves.includes("b1") && playerMoves.includes("b2") && playerMoves.includes("b3") ||
@@ -54,6 +56,7 @@ class Game {
     } else if (this.holdWinningMoves(this.player2Moves)) {
       this.runWinningSequences();
     }
+    changePlayerTurn();
   }
   checkForDraw() {
     if (this.turnCounter === 9) {
