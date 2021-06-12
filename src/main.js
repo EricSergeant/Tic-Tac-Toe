9ee7335af7d3016b51bdcd7e43025ee1920b2e81
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-06-12 14:54:23 -0500

    Add console log tests to all main.js functions

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ var player2WinCount = document.querySelector('.player-2-wins');
 var dsiplayPlayerTurnId = document.querySelector('.player-name-turn-display');
 var displayPlayerTurnToken = document.querySelector('.player-icon-turn-display');
 var resetScoreButton = document.querySelector('.reset-scores-to-zero');
-
-// ...board button identifiers below...
 var a1 = document.querySelector('.a1');
 var a2 = document.querySelector('.a2');
 var a3 = document.querySelector('.a3');
@@ -19,41 +17,62 @@ var c2 = document.querySelector('.c2');
 var c3 = document.querySelector('.c3');
 
 // 👇 global variables go here...👇
-var game = new Game;
+var currentGame = new Game;
 //will this instantiate both Game and P1/P2 ???
 
 
 // 👇 event listeners go here 👇
-window.addEventListener('load', startGameOnLoad());
-
+window.addEventListener('load', startGameOnLoad);
+resetScoreButton.addEventListener('click', resetScores);
 // need to add:
-//  reset resetScoreButton function
 //  event listener for each button in board
-
+// OPTION ONE:
+// a1.addEventListener('click', function(event) {
+//   selectSquareChoice();
+// })
+// OPTION TWO:
+gameBoard.addEventListener('click', selectSquareChoice);
 
 // 👇 functions and event handlers go here 👇
-startGameOnLoad() {
-  // need to check on saved data and repopulate scores accordingly
+function startGameOnLoad() {
+  console.log('clicked startGameOnLoad');
 }
+// need to check on saved data and repopulate scores accordingly
 
-renderWinCountDisplay() {
+function renderWinCountDisplay() {
   // shows the number of wins for each player
+  console.log('clicked renderWinCountDisplay');
 }
 
-showWinningMessage() {
+function showWinningMessage() {
   // will show which player won along with their icon
+  console.log('clicked showWinningMessage');
 }
 
-showDrawMessage() {
+function showDrawMessage() {
   // will show a message in case of a draw
+  console.log('clicked showDrawMessage');
 }
 
-showPlayerTurn() {
+function showPlayerTurn() {
   // will display the current player's turn and icon
+  console.log('clicked showPlayerTurn');
 }
 
-selectSquareChoice() {
+function selectSquareChoice() {
   // will allow button(square) to be selected
-  // will disable button for re-click
   // will show icon in selected button
+  // will game.recordPlayerMove(moveClass)
+  console.log('clicked selectSquareChoice');
+}
+
+function disableSelectSquare() {
+    // will disable button for re-click
+    console.log('clicked disableSelectSquare');
+}
+
+function resetScores() {
+    console.log('clicked resetScores');
+  // functionality for reset scores button, puts all back to zero wins in data model
+  // need to point to new function in game.js probably ???
 }
